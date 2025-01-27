03b1e34d1ecd446675623b2483ffea3258f0bd0e
nicodigital <nicolook.pro@gmail.com>
2025-01-27 13:07:25 -0300

    Works Animation

--- resources/js/module/gsap.js
+++ resources/js/module/gsap.js
@@ -1,29 +1,29 @@
 import { gsap } from "gsap";
-import SplitType from 'split-type'
 
 export function removeOnce(container) {
   setTimeout(() => { // Removemos la clase "once" para que saber que ya se ha cargado el ONCE
     document.querySelector('body').classList.remove('once');
-  }, 3000)
+  },5000)
 }
 
+/* Menu Intro */
 export function menuIntro() {
 
   return gsap.to("menu a span", {
     duration: 1,
     y: 0,
     stagger: 0.1,
-  }, "=0.5");
+  });
 
 }
-
+/* Lang Switcher */
 export function langSwitcher() {
-  const tl = gsap.timeline();
+  const tl = gsap.timeline()
 
   return tl.to(".lang-switcher a:nth-child(1)", {
     x: 0,
     duration: .5,
-  }, "3.5")
+  }, "3")
   .to(".lang-switcher span", {
     width: "100%",
     duration: .5,
@@ -34,33 +34,147 @@ export function langSwitcher() {
   }, "-=.5");
 
 }
-
+/* Home Intro */
 export function homeIntro(container) {
-  const tl = gsap.timeline();
-
-  let txtNico = new SplitType("#txtNico");
-  let txtAbout = new SplitType("#txtAbout", { types: "lines" });
+  const tl = gsap.timeline()
 
   return tl.to('h1', {
     y: 0, 
     duration: .75,
-    delay: 1
-  })
-  .to("p .line", {
+    delay: .75
+  }, "-=.2")
+  .to("line span", {
     y: 0,
-    stagger: 0.1,
-    duration: .7,
-    clipPath: "polygon(0 100%, 100% 100%, 100% 0, 0 0)",
+    stagger: 0.15,
+    duration: .6,
   }, "-=1")
   .to("#btnWorks", {
     y: 0, 
-    duration: .75,
-  })
-  .to(".char", {
+    duration: .65,
+  }, "-=.2")
+  .to(".chars span", {
     y: 0,
     stagger: 0.05,
-    duration: 0.1
-  }, "-=0.5");
+    duration: 0.1,
+  }, "-=1")
+}
+
+/* Home Outro */
+export function homeOutro(container) {
+  const tl = gsap.timeline()
+
+  return tl.to('h1', {
+    y: "-3rem",  
+    duration: .75,
+  })
+  .to("line span", {
+    y: "-3rem", 
+    stagger: -0.1,
+    duration: .7,
+  }, "-=.75")
+  .to("#btnWorks", {
+    y: "-3rem", 
+    duration: .75,
+  }, "-=.75")
+  .to(".chars span", {
+    y: "-20rem",
+    stagger: {
+      each: -0.05,
+      from: "end",
+    },
+    duration: 0.1,
+  }, "-=1");
+}
+
+/* Logo Intro */
+export function logoIntro(){
+
+  const tl = gsap.timeline()
+
+  return tl.to("logo .name span", {
+    y: 0,
+    duration: .6,
+  }).to("logo .claim span", {
+    y: 0,
+    duration: .6
+  }, "-=.4");
+
+}
+
+/* logo outro */
+export function logoOutro(container){
+
+  const tl = gsap.timeline()
+  // const logo = container.querySelector('logo .name span')
+  // const claim = container.querySelector('logo .claim span')
+
+  return tl.to("logo .name span", {
+    yPercent: 110,
+    duration: .6,
+  }).to("logo .claim span", {
+    yPercent: -110,
+    duration: .6
+  }, "-=.4");
+
+}
+
+/* BigTitleIntro */
+export function bigTitleIntro(container) {
+  return gsap.to(".title.chars span", {
+    y: 0,
+    stagger: 0.05,
+    duration: 0.2,
+  }, "-=.65");
+}
+
+export function bigTitleOutro(container) {
+  return gsap.to(".title.chars span", {
+    y: "-20rem",
+    stagger: {
+      each: -0.05,
+      from: "end",
+    },
+    duration: 0.2,
+  })
+}
+
+/* Works Intro */
+export function worksIntro(){
+  const tl = gsap.timeline()
+
+  return tl.to("line span", {
+    y: 0,
+    stagger: 0.1,
+    duration: .7,
+  }, "-=.5").to(".loop card", {
+    y: 0,
+    stagger: 0.1,
+    duration: .65,
+    ease: "power4.In"
+  }, "-=.5");
+
+}
+
+/* Works Outro */
+export function worksOutro() {
+  const tl = gsap.timeline()
+
+  return tl.to("line span", {
+    y: "-3rem",
+    stagger: {
+      each: -0.1,
+      from: "end",
+    },
+    duration: .7,
+  }, "-=.5").to(".loop card", {
+    yPercent: 100,
+    stagger: {
+      each: -0.1,
+      from: "end",
+    },
+    duration: .65,
+    ease: "power4.In"
+  })
 }
 
 // export function animEnter( container ) {
--- resources/js/module/transitions.js
+++ resources/js/module/transitions.js
@@ -1,6 +1,6 @@
 import barba from '@barba/core'
 import barbaPrefetch from '@barba/prefetch'
-import { removeOnce, homeIntro, menuIntro, langSwitcher } from './gsap.js'
+import { removeOnce, homeIntro, homeOutro, menuIntro, langSwitcher, logoIntro, logoOutro, bigTitleIntro, worksIntro, worksOutro, bigTitleOutro } from './gsap.js'
 import functions from '../functions.js'
 
 function transitions(deviceData) {
@@ -14,17 +14,18 @@ function transitions(deviceData) {
     sync: true,
     timeout: 10000,
     transitions: [
+      // TO HOME
       {
-        name: 'home',
+        name: 'to-home',
         to: { namespace: 'home' },
-        enter() {
-          console.log('ENTER HOME')
+        enter({ next }) {
+          console.log('ENTER TO HOME')
           homeIntro()
           currentCleanup = functions(next.container, deviceData)
           removeOnce()
         },
         once({ next }) {
-          console.log('ONCE HOME')
+          console.log('ONCE TO HOME')
           menuIntro()
           homeIntro()
           langSwitcher()
@@ -32,11 +33,62 @@ function transitions(deviceData) {
           removeOnce()
         },
         leave: ({ next, current }) => {
-          console.log('LEAVE HOME')
+          console.log('LEAVE -> TO HOME')
+          return new Promise(resolve => {
 
+            logoOutro(current.container)
+            bigTitleOutro()
+            worksOutro()
+
+            setTimeout(() => {
+              resolve()
+            }, 1400)
+
+          })
         },
       },
-      { // DEFAULT
+      // TO WORKS
+      { 
+        name: 'to-works',
+        to: { namespace: 'works' },
+        once({ next }) {
+          console.log('ONCE TO WORKS')
+          logoIntro()
+          bigTitleIntro()
+          worksIntro()
+          currentCleanup = functions(next.container, deviceData)
+          removeOnce()
+        },
+
+        leave: ({ next, current }) => {
+          console.log('LEAVE -> TO WORKS')
+          
+          // Limpiar funciones anteriores
+          if (currentCleanup && typeof currentCleanup === 'function') {
+            currentCleanup()
+          }
+
+          return new Promise(resolve => {
+            homeOutro()
+            setTimeout(() => {
+              resolve()
+            }, 1400)
+          })
+        },
+
+        enter: ({ next }) => {
+          console.log('ENTER TO WORKS')
+          logoIntro()
+          bigTitleIntro()
+          worksIntro()
+          // Inicializar nuevas funciones después de que el DOM esté listo
+          setTimeout(() => {
+            currentCleanup = functions(next.container, deviceData)
+          }, 300)
+        }
+      },
+      // DEFAULT
+      { 
         name: 'default',
         once({ next }) {
           console.log('ONCE DEFAULT')
@@ -45,7 +97,7 @@ function transitions(deviceData) {
         },
 
         leave: ({ next, current }) => {
-          console.log('LEAVE DEFAULT')
+          console.log('LEAVE -> DEFAULT')
 
           // Limpiar funciones anteriores
           if (currentCleanup && typeof currentCleanup === 'function') {
