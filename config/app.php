d4dbea978f83b30bfe0c682483394b1c2b8fdcfe
Nicolás González <nicolook.pro@gmail.com>
2025-02-21 09:45:01 -0300

    Try to fix paths

--- config/app.php
+++ config/app.php
@@ -52,8 +52,8 @@ return [
     |
     */
 
-    // 'url' => env('APP_URL', 'http://localhost'),
-    'url' => env('APP_URL', 'https://nicolas-gonzalez.netlify.app'),
+    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'https://nicolas-gonzalez.netlify.app'),
 
     /*
     |--------------------------------------------------------------------------
